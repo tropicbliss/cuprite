fbdbf9a2a45b83674cbf4e773d0e13a9952bb99a
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2021-12-09 23:29:36 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,6 @@ use anyhow::{bail, Context, Result};
 #[async_std::main]
 async fn main() -> Result<()> {
     let args = cli::Args::new();
-    let manipulator = file_io::FileManipulator::new(
-        args.input_dir,
-        args.output_dir,
-        usize::from(args.max_backups),
-        args.compression_level,
-    );
     let mut server = minecraft::Server::new(args.rcon_port, args.rcon_password)
         .await
         .with_context(|| "Failed to connect to server")?;
@@ -22,6 +16,12 @@ async fn main() -> Result<()> {
         .connect()
         .await
         .with_context(|| "Failed to send initial RCON messages")?;
+    let manipulator = file_io::FileManipulator::new(
+        args.input_dir,
+        args.output_dir,
+        usize::from(args.max_backups),
+        args.compression_level,
+    );
     if let Err(e) = manipulator
         .truncate_target_dir()
         .with_context(|| "Failed to truncate target directory")
